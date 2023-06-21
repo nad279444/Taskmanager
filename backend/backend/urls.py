96638cad31994653a076bbae895a721abe47bca6
nad279444 <nardfrimpong@gmail.com>
2023-06-21 12:02:04 +0000

    registered all routes in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from task_api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'tasks',views.TaskView,'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
 ]
