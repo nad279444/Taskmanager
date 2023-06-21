fb3ada85ab1139a38029e5c1bbad3a9eae739e1a
nad279444 <nardfrimpong@gmail.com>
2023-06-21 11:04:36 +0000

    created a super user for database and api testing

--- backend/task_api/admin.py
+++ backend/task_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display=("title","description","completed")
+
+
+#Register the Model
+
+admin.site.register(Task,TaskAdmin)
